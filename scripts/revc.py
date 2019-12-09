43f453917ca487dc23cda49f5d67944b52889178
Asoretzu <asoretzu@gmail.com>
2019-12-09 16:45:42 -0600

    Now with docstrings.

--- scripts/revc.py
+++ scripts/revc.py
@@ -1,29 +1,34 @@
-# ID = "REVC"
-# PROJECT = "Complementing a Strand of DNA"
-
-
-# In DNA strings, symbols 'A' and 'T' are complements of each other, as are
-# 'C' and 'G'.
-
-# The reverse complement of a DNA string "s" is the string sc formed by
-# reversing the symbols of "s", then taking the complement of each symbol
-# (e.g., the reverse complement of "GTCA" is "TGAC").
-
-# Given: A DNA string "s" of length at most 1000 bp.
-
-# Return: The reverse complement "sc" of "s".
-
-# Sample Dataset
-# AAAACCCGGT
-
-# Sample Output
-# ACCGGGTTTT
-
-
 from services import fasta
 
 
 def revc(file_name):
+    """
+    Prints the reverse complement of a given string.
+
+    Keyword arguments:
+    file_name -- The path of the txt file to be parsed.
+
+
+    Complementing a Strand of DNA
+
+    In DNA strings, symbols 'A' and 'T' are complements of each other, as are
+    'C' and 'G'.
+
+    The reverse complement of a DNA string "s" is the string sc formed by
+    reversing the symbols of "s", then taking the complement of each symbol
+    (e.g., the reverse complement of "GTCA" is "TGAC").
+
+    Given: A DNA string "s" of length at most 1000 bp.
+
+    Return: The reverse complement "sc" of "s".
+
+    Sample Dataset
+    AAAACCCGGT
+
+    Sample Output
+    ACCGGGTTTT
+    """
+
     dna = fasta.get(file_name)
     rev_dna = dna[:: -1]
     com_dna = ""
--- scripts/rna.py
+++ scripts/rna.py
@@ -1,27 +1,34 @@
-# ID = "RNA"
-# PROJECT = "Transcribing DNA into RNA"
-
-# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G',
-# and 'U'.
-
-# Given a DNA string "t" corresponding to a coding strand, its transcribed RNA
-# string "u" is formed by replacing all occurrences of 'T' in t with 'U' in u.
-
-# Given: A DNA string "t" having length at most 1000 nt.
-
-# Return: The transcribed RNA string of t.
-
-# Sample Dataset
-# GATGGAACTTGACTACGTAAATT
-
-# Sample Output
-# GAUGGAACUUGACUACGUAAAUU
-
-
 from services import fasta
 
 
 def rna(file_name):
+    """
+    Prints the transcribed RNA string of a given string.
+
+    Keyword arguments:
+    file_name -- The path of the txt file to be parsed.
+
+
+    Transcribing DNA into RNA
+
+    An RNA string is a string formed from the alphabet containing 'A', 'C',
+    'G', and 'U'.
+
+    Given a DNA string "t" corresponding to a coding strand, its transcribed
+    RNA string "u" is formed by replacing all occurrences of 'T' in t with 'U'
+    in u.
+
+    Given: A DNA string "t" having length at most 1000 nt.
+
+    Return: The transcribed RNA string of t.
+
+    Sample Dataset
+    GATGGAACTTGACTACGTAAATT
+
+    Sample Output
+    GAUGGAACUUGACUACGUAAAUU
+    """
+
     dna = fasta.get(file_name)
     rna = ""
 
