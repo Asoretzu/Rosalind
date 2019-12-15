9ceea30bb9b307ae2bfaae1267d6df6aa2365f9a
Asoretzu <asoretzu@gmail.com>
2019-12-14 20:07:08 -0600

    Functions callable from the main file.

--- scripts/fibd.py
+++ scripts/fibd.py
@@ -1,33 +1,39 @@
-# ID = "FIBD"
-# PROJECT = "Mortal Fibonacci Rabbits"
-
-# Recall the definition of the Fibonacci numbers from “Rabbits and Recurrence
-# Relations”, which followed the recurrence relation Fn=Fn−1+Fn−2 and assumed
-# that each pair of rabbits reaches maturity in one month and produces a single
-# pair of offspring (one male, one female) each subsequent month.
-
-# Our aim is to somehow modify this recurrence relation to achieve a dynamic
-# programming solution in the case that all rabbits die out after a fixed
-# number of months. See Figure 4 for a depiction of a rabbit tree in which
-# rabbits live for three months (meaning that they reproduce only twice before
-# dying).
-
-# Given: Positive integers n≤100 and m≤20.
-
-# Return: The total number of pairs of rabbits that will remain after the n-th
-# month if all rabbits live for m months.
-
-# Sample Dataset
-# 6 3
-
-# Sample Output
-# 4
-
-
 from services import fasta
 
 
 def fibd(file_name):
+    """
+    Prints the number of pairs of rabbits that will remain after n-th months.
+
+    Keyword arguments:
+    file_name -- The path of the txt file to be parsed.
+
+
+    Mortal Fibonacci Rabbits
+
+    Recall the definition of the Fibonacci numbers from “Rabbits and Recurrence
+    Relations”, which followed the recurrence relation Fn=Fn−1+Fn−2 and assumed
+    that each pair of rabbits reaches maturity in one month and produces a
+    single pair of offspring (one male, one female) each subsequent month.
+
+    Our aim is to somehow modify this recurrence relation to achieve a dynamic
+    programming solution in the case that all rabbits die out after a fixed
+    number of months. See Figure 4 for a depiction of a rabbit tree in which
+    rabbits live for three months (meaning that they reproduce only twice
+    before dying).
+
+    Given: Positive integers n≤100 and m≤20.
+
+    Return: The total number of pairs of rabbits that will remain after the
+    n-th month if all rabbits live for m months.
+
+    Sample Dataset
+    6 3
+
+    Sample Output
+    4
+    """
+
     data = fasta.get(file_name)
     data = data.split()
 
