4fc93e89fc928142630de0dd128c0f255dab54e8
Asoretzu <asoretzu@gmail.com>
2019-12-25 17:56:40 -0600

    Improved the LCSM function. Merry Christmas. :)

--- scripts/lcsm.py
+++ scripts/lcsm.py
@@ -1,72 +1,67 @@
-# ID = "LCSM"
-# PROJECT = "Finding a Shared Motif"
-
-# A common substring of a collection of strings is a substring of every member
-# of the collection. We say that a common substring is a longest common
-# substring if there does not exist a longer common substring. For example,
-# "CG" is a common substring of "ACGTACGT" and "AACCGTATA", but it is not as
-# long as possible in this case, "CGTA" is a longest common substring of
-# "ACGTACGT" and "AACCGTATA".
-
-# Note that the longest common substring is not necessarily unique for a simple
-# example, "AA" and "CC" are both longest common substrings of "AACC" and
-# "CCAA".
-
-# Given: A collection of k (k≤100) DNA strings of length at most 1 kbp each in
-# FASTA format.
-
-# Return: A longest common substring of the collection. (If multiple solutions
-# exist, you may return any single solution.)
-
-# Sample Dataset
-
-# >Rosalind_1
-# GATTACA
-# >Rosalind_2
-# TAGACCA
-# >Rosalind_3
-# ATACA
-
-# Sample Output
-# AC
-
-
 from services import fasta
 
 
-def LCSM():
-    # dataset = fasta.get("TXT/lalo.txt")
-    dataset = fasta.get("TXT/rosalind_lcsm.txt")
+def lcsm(file_name):
+    """
+    Prints the longest common substring of a collection.
+
+    Keyword arguments:
+    file_name -- The path of the txt file to be parsed.
+
+
+    PROJECT = "Finding a Shared Motif"
+
+    A common substring of a collection of strings is a substring of every
+    member of the collection. We say that a common substring is a longest
+    common substring if there does not exist a longer common substring. For
+    example, "CG" is a common substring of "ACGTACGT" and "AACCGTATA", but it
+    is not as long as possible in this case, "CGTA" is a longest common
+    substring of "ACGTACGT" and "AACCGTATA".
+
+    Note that the longest common substring is not necessarily unique for a
+    simple example, "AA" and "CC" are both longest common substrings of "AACC"
+    and "CCAA".
+
+    Given: A collection of k (k≤100) DNA strings of length at most 1 kbp each
+    in FASTA format.
+
+    Return: A longest common substring of the collection. (If multiple
+    solutions exist, you may return any single solution.)
+
+    Sample Dataset
+
+    >Rosalind_1
+    GATTACA
+    >Rosalind_2
+    TAGACCA
+    >Rosalind_3
+    ATACA
+
+    Sample Output
+    AC
+    """
+
+    dataset = fasta.get(file_name)
     dataset = sorted(dataset[1], key=len)
+    seq = dataset[1:]
+    srtd_seq = sorted(seq, key=len)
 
-    pattern = dataset[0]
-    sequences = dataset[1:]
+    short_seq = srtd_seq[0]
+    comp_seq = srtd_seq[1:]
+    motif = ""
 
-    find = 0
-    found = []
+    for i in range(len(short_seq)):
+        for j in range(i, len(short_seq)):
+            m = short_seq[i: j+1]
+            shared_motif = False
 
-    # Asign a substring
-    for i in range(0, len(pattern)):
-        for j in range(len(pattern), i+1, -1):
+            for seq in comp_seq:
+                if m in seq:
+                    shared_motif = True
+                else:
+                    break
 
-            # Search for shared motifs
-            for seq in sequences:
-                if pattern[i: j+1] in seq:
-                    find = find + 1
+            if shared_motif and len(m) > len(motif):
+                motif = m
 
-            # Store the motif, if it is shared in all the sequences
-            if find == len(sequences) and len(pattern[j: i+1]) < len(pattern):
-                print("Found: {}.".format(pattern[i: j+1]))
-                found.append(pattern[i: j+1])
-            find = 0
-
-    # Print the longest shared motif
-    found = sorted(found, key=len)
-    print("This is the one: {}.".format(found[-1]))
-    print("")
-
-    # CGCACGACACTAGCCCTGTTTAGATAAGCTGATCCGGAAAATGCGTGAGGCTGTGCTCACATTAACAGAACCTCTCGGTTCGGAAAGAA
-
-
-if __name__ == "__main__":
-    LCSM()
+    print(motif)
