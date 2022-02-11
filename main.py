f153de61635d4b3d21cb5856f9dcdbbd09208e55
asoretzu <asoretzu@gmail.com>
2022-02-11 14:06:11 -0600

    Improving the main file for accepting arguments via console.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-# Import the script file of the problem you are trying to solve.
-from scripts.prtm import prtm
+#!/usr/bin/python
+
+import sys
+
+from scripts import *
+
+
+# Main function with options
+def main(work):
+    try:
+        if work[1] == 'dna':
+            print(dna.dna(work[2]))
+        else:
+            print(f"The module <{work[1]}> doesn't exist.")
+    except:
+        print('Invalid arguments received.')
 
 
 # Call the function of the script
 if __name__ == "__main__":
-    prtm("txt/rosalind_prtm.txt")
+    args = len(sys.argv) - 1
+
+    if args == 2:
+            main(sys.argv)
+    else:
+        print(f'Invalid number of arguments. Expected 2, but received {args}.')
