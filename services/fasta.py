f15df4e4a5779ec2972c3a980fd04d5ff563b37d
asoretzu <asoretzu@gmail.com>
2022-02-11 14:15:59 -0600

    Removing innecesary text and adding other comments.

--- services/fasta.py
+++ services/fasta.py
@@ -4,6 +4,7 @@ def get(file_name, remove_new_line=True):
     fasta_codes = []
     string = ""
 
+    # Open the file and parsing every string
     with open(file_name, mode="r") as f:
         for line in f:
             # Check for FASTA IDs
@@ -11,20 +12,20 @@ def get(file_name, remove_new_line=True):
                 line = line[1: -1]
                 fasta_ids.append(line)
 
-                # Asign every FASTA CODE
+                # Asign every FASTA code
                 if len(fasta_ids) == 1:
                     pass
                 else:
                     fasta_codes.append(string)
                     string = ""
             else:
-                # Concatenate all the FASTA CODE
+                # Concatenate all the FASTA code
                 if remove_new_line:
                     string = string + line[0:-1]
                 else:
                     string = string + line
 
-        # Asign the last FASTA CODE
+        # Asign the last FASTA code
         fasta_codes.append(string)
 
     if not fasta_ids:
